6d5f19a0272fa00c8d266ad9d6cd245f3927d83a
Jamie Atkinson <jabbslad@gmail.com>
2012-06-12 01:39:07 +0100

    An important update

--- HL7Interrogator/src/HI/HL7Interrogator.java
+++ HL7Interrogator/src/HI/HL7Interrogator.java
@@ -14,6 +14,6 @@ public class HL7Interrogator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Finish uz off!");
     }
 }
